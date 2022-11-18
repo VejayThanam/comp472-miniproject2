02633a71ff816f4294c737586bef6abb4e373059
vithushen <78235386+vithushen@users.noreply.github.com>
2022-11-18 01:42:48 -0500

    Created 2D array given list of car info

--- app.py
+++ app.py
@@ -21,9 +21,31 @@ with open('sample-input.txt') as f_in:
 
     #s1.get_next_state()
 
+    new_grid = [["." for x in range(6)] for y in range(6)]
+
     for car in s1.cars:
         car.print_car()
         print('\n')
+
+    for car in s1.cars:
+        for i in range(6):
+            for j in range(6):
+                if car.row==i and car.col==j:
+                    new_grid[i][j]=car.letter
+                    if car.horiz and car.length==2:           
+                            new_grid[i][j+1]=car.letter
+                    elif car.horiz:
+                            new_grid[i][j+1]=car.letter
+                            new_grid[i][j+2]=car.letter
+                    elif not car.horiz and car.length==2:
+                        new_grid[i+1][j]=car.letter
+                    elif not car.horiz:
+                        new_grid[i+1][j]=car.letter
+                        new_grid[i+2][j]=car.letter
+
+        
+    print(new_grid)
+
 
     # for state in listOfStates:
     #     print(state.get_state_string())
